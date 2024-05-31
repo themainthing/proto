6a95d2a04f3fd8e82dae6f4b5ca8824dde8ad3f6
Anton Chertovich <aannttoonn09@gmail.com>
2024-05-31 16:34:51 +0300

    sffsfsfs

--- protogen/go/bank/service_grpc.pb.go
+++ protogen/go/bank/service_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v5.26.1
-// source: proto/bank/service.proto
+// source: proto/account/service.proto
 
 package bank
 
@@ -18,88 +18,88 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// BankHandlerClient is the client API for BankHandler service.
+// AccountHandlerClient is the client API for AccountHandler service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type BankHandlerClient interface {
+type AccountHandlerClient interface {
 	GetCurrentBalance(ctx context.Context, in *CurrentBalanceRequest, opts ...grpc.CallOption) (*CurrentBalanceResponse, error)
 }
 
-type bankHandlerClient struct {
+type accountHandlerClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewBankHandlerClient(cc grpc.ClientConnInterface) BankHandlerClient {
-	return &bankHandlerClient{cc}
+func NewAccountHandlerClient(cc grpc.ClientConnInterface) AccountHandlerClient {
+	return &accountHandlerClient{cc}
 }
 
-func (c *bankHandlerClient) GetCurrentBalance(ctx context.Context, in *CurrentBalanceRequest, opts ...grpc.CallOption) (*CurrentBalanceResponse, error) {
+func (c *accountHandlerClient) GetCurrentBalance(ctx context.Context, in *CurrentBalanceRequest, opts ...grpc.CallOption) (*CurrentBalanceResponse, error) {
 	out := new(CurrentBalanceResponse)
-	err := c.cc.Invoke(ctx, "/bank.BankHandler/GetCurrentBalance", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/account.AccountHandler/GetCurrentBalance", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// BankHandlerServer is the server API for BankHandler service.
-// All implementations must embed UnimplementedBankHandlerServer
+// AccountHandlerServer is the server API for AccountHandler service.
+// All implementations must embed UnimplementedAccountHandlerServer
 // for forward compatibility
-type BankHandlerServer interface {
+type AccountHandlerServer interface {
 	GetCurrentBalance(context.Context, *CurrentBalanceRequest) (*CurrentBalanceResponse, error)
-	mustEmbedUnimplementedBankHandlerServer()
+	mustEmbedUnimplementedAccountHandlerServer()
 }
 
-// UnimplementedBankHandlerServer must be embedded to have forward compatible implementations.
-type UnimplementedBankHandlerServer struct {
+// UnimplementedAccountHandlerServer must be embedded to have forward compatible implementations.
+type UnimplementedAccountHandlerServer struct {
 }
 
-func (UnimplementedBankHandlerServer) GetCurrentBalance(context.Context, *CurrentBalanceRequest) (*CurrentBalanceResponse, error) {
+func (UnimplementedAccountHandlerServer) GetCurrentBalance(context.Context, *CurrentBalanceRequest) (*CurrentBalanceResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetCurrentBalance not implemented")
 }
-func (UnimplementedBankHandlerServer) mustEmbedUnimplementedBankHandlerServer() {}
+func (UnimplementedAccountHandlerServer) mustEmbedUnimplementedAccountHandlerServer() {}
 
-// UnsafeBankHandlerServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to BankHandlerServer will
+// UnsafeAccountHandlerServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to AccountHandlerServer will
 // result in compilation errors.
-type UnsafeBankHandlerServer interface {
-	mustEmbedUnimplementedBankHandlerServer()
+type UnsafeAccountHandlerServer interface {
+	mustEmbedUnimplementedAccountHandlerServer()
 }
 
-func RegisterBankHandlerServer(s grpc.ServiceRegistrar, srv BankHandlerServer) {
-	s.RegisterService(&BankHandler_ServiceDesc, srv)
+func RegisterAccountHandlerServer(s grpc.ServiceRegistrar, srv AccountHandlerServer) {
+	s.RegisterService(&AccountHandler_ServiceDesc, srv)
 }
 
-func _BankHandler_GetCurrentBalance_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _AccountHandler_GetCurrentBalance_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CurrentBalanceRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(BankHandlerServer).GetCurrentBalance(ctx, in)
+		return srv.(AccountHandlerServer).GetCurrentBalance(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/bank.BankHandler/GetCurrentBalance",
+		FullMethod: "/account.AccountHandler/GetCurrentBalance",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(BankHandlerServer).GetCurrentBalance(ctx, req.(*CurrentBalanceRequest))
+		return srv.(AccountHandlerServer).GetCurrentBalance(ctx, req.(*CurrentBalanceRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// BankHandler_ServiceDesc is the grpc.ServiceDesc for BankHandler service.
+// AccountHandler_ServiceDesc is the grpc.ServiceDesc for AccountHandler service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var BankHandler_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "bank.BankHandler",
-	HandlerType: (*BankHandlerServer)(nil),
+var AccountHandler_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "account.AccountHandler",
+	HandlerType: (*AccountHandlerServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "GetCurrentBalance",
-			Handler:    _BankHandler_GetCurrentBalance_Handler,
+			Handler:    _AccountHandler_GetCurrentBalance_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/bank/service.proto",
+	Metadata: "proto/account/service.proto",
 }
